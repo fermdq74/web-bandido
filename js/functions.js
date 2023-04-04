522121ccac58f0b2c65e5bcf4b51c91a5ea0b57a
Fernando Petroli <fernando@tectonica.co>
2023-04-04 16:13:23 -0300

    General updates

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,2 @@
 // Global functions
-function progressBar() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("progressBar").style.width = scrolled + "%";
-    document.getElementById("progressOval").style.marginLeft = scrolled + "%";
-}
+
