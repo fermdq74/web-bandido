bfe874f83decfc6406e44d5d6198c40f9a6b581d
Fernando Petroli <fernando@tectonica.co>
2023-04-10 17:01:04 -0300

    Scroll v2

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,21 @@
 // Global functions
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
+function animateLeft(obj, from, to){
+
+        var box = obj;
+        //box.style.marginLeft = from + "px";
+        box.style.transform = "translate(" + from + ",-50%)";
+        setTimeout(function(){
+            animateLeft(obj, from - 1, to);
+        }, 25)
+
+ }
